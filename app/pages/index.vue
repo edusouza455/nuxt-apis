9a3d0c13ec0798498f22fd06cfcae85d34b3b9ff
edusouza455 <edu.testes456@gmail.com>
2025-08-01 18:52:26 -0300

    centralizaçao do conteudo e estilizacao da lista

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h1>testando APIs</h1>
+  <div class="flex flex-col items-center justify-start min-h-screen  bg-gray-100">
+    <h1 class="text-2xl font-bold m-10">testando APIs</h1>
 
-    <ul>
+    <ul class="list-disc">
       <li><nuxt-link to="/use-fetch">Go to Use Fetch Page</nuxt-link></li>
       <li><nuxt-link to="/use-async-data">Go to Use Async Data Page</nuxt-link></li>
       <li><nuxt-link to="/o-fetch">Go to O Fetch Page</nuxt-link></li>
