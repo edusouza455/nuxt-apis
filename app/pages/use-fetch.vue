f52f84b8d34bf412823c0b00d0c0b821ae7ce89a
edusouza455 <edu.testes456@gmail.com>
2025-08-04 20:17:23 -0300

    atualizacao de desing

--- app/pages/use-fetch.vue
+++ app/pages/use-fetch.vue
@@ -1,22 +1,32 @@
 <template>
-    <h1>Use Fetch Page</h1>
-    <button @click="refresh">recarregar dados</button>   
+    <div class="bg-gray-100 min-h-screen p-8 ">
+        <div class="max-w-3xl mx-auto bg-white p-6 mb-6 rounded-lg shadow-lg flex flex-col items-center ">
+             <h1>Use Fetch Page</h1>
+            <button @click="refresh">recarregar dados</button>   
 
-    <div v-if="pending">carregando...</div>
-    <div v-else-if="error">Erro ao carregar dados: {{ error.message }}</div>
+            <div v-if="pending">carregando...</div>
+            <div v-else-if="error">Erro ao carregar dados: {{ error.message }}</div>
+
+            
+            <p class='bg-gray-200 p-8 m-4' v-else>        
+                {{ charmander }}
+            </p>
+
+            <div>
+                <select v-model='selectdPokemon'>
+                    <option value="charmander">Charmander</option>
+                    <option value="bulbasaur">Bulbasaur</option>
+                    <option value="squirtle">Squirtle</option>
+                </select>
+            </div>
+            
+        </div>
+       
+
+    </div>
+    
 
     
-    <pre v-else>        
-        {{ charmander }}
-    </pre>
-
-    <div>
-        <select v-model='selectdPokemon'>
-            <option value="charmander">Charmander</option>
-            <option value="bulbasaur">Bulbasaur</option>
-            <option value="squirtle">Squirtle</option>
-        </select>
-    </div>
 
 </template>
 
